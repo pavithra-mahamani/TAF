ec01424091bbd5b0bfba22c117107e1bc9172243
pavithra-mahamani <42983325+pavithra-mahamani@users.noreply.github.com>
2019-09-20 15:49:33 -0700

    Update xdcrbasetest.py

--- pytests/xdcr/xdcrbasetest.py
+++ pytests/xdcr/xdcrbasetest.py
@@ -7,7 +7,8 @@ class XDCRNewBaseTest(BaseTestCase):
         super(XDCRNewBaseTest, self).setUp()
         self.clusters = self.get_clusters()
         for cluster in self.clusters:
-            cluster.cluster_util.add_all_nodes_then_rebalance(cluster.servers[1:])
+            cluster_util = ClusterUtils(cluster, self.task_manager)
+            cluster_util.add_all_nodes_then_rebalance(cluster.servers[1:])
         self.task = self.get_task()
         self.taskmgr = self.get_task_mgr()
 
